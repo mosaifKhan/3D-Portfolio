e9ff113eb3fecc407fb33dca430dc72521a6c947
Mosaif Khan <52195172+mosaifKhan@users.noreply.github.com>
2023-10-03 02:55:21 +0530

    first commit

---
+++ src/components/Copyright.jsx
@@ -0,0 +1,14 @@
+import { styles } from "../styles";
+
+const CopyRight = () => {
+    return(
+        <footer className='flex flex-col'>
+            <div
+              className='bg-black-100 text-secondary border-none text-center'>
+                Copyright © 2023 Musaib Khan | All rights reserved.
+            </div>
+          </footer>
+    )
+}
+
+export default CopyRight;
